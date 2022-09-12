fb16ff79ef45c68e71ce33ac54e5562378cc6791
hadar <hadar.zaguri@gmail.com>
2022-09-12 15:23:04 +0300

    phone-ipad-cellphone

--- src/componenets/AnimationComputerMovement/AnimMovComputer.jsx
+++ src/componenets/AnimationComputerMovement/AnimMovComputer.jsx
@@ -3,15 +3,23 @@ import {images} from '../../constants/index'
 const AnimMovComputer = () => {
   return (
     <div className='height-vh'>
-        <img className='img-screen shadow-macbook' src={images.macbook_shadow}/>
-        <img className='img-screen macbook' src={images.macbook}/>
-        <img className='img-screen macbook-screen' src={images.mac_screen}/>
+<div> 
+    <img className='img-screen shadow-macbook' src={images.macbook_shadow}/>
+    <img className='img-screen macbook' src={images.macbook}/>
+    <img className='img-screen macbook-screen' src={images.mac_screen}/>
+</div>
 
+    <div>
         <img className='img-screen shadow-ipad' src={images.ipad_shadow}/>
         <img className='img-screen ipad' src={images.ipad}/>
         <img className='img-screen ipad-screen' src={images.ipad_screen}/>
-
-        {/* <img className='img-screen shadow-iphone' src={images.iphone_shadow}/> */}
+    </div>
+        
+    <div>
+        <img className='img-screen shadow-iphone' src={images.iphone_shadow}/>
+        <img className='img-screen iphone' src={images.iphone}/>
+        <img className='img-screen iphone-screen' src={images.iphone_screen}/>
+    </div>
 
 
 
